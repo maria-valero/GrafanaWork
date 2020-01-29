3415022d525d732d0472a302d2bf2486a867173a
Maria <maria.valero@uga.edu>
2020-01-29 10:50:51 -0500

    scripts modification

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # GrafanaWork
+
+This scripts copy a dashboard in grafana, made a modification in their JSON file, and then import again the dashboard.
+
+## Usage
+
+### export.sh
+
+./exporth
+
+### modify.py
+
+python modify.py [NEW_MAC] [DASHBOARD_TO_COPY]
+
+### import.sh
+
+./import [DASHBOARD_NAME]
--- export.sh
+++ export.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#KEY="XXXXXXXX"
 KEY="XXXXXXXXX"
 #HOST="localhost:3000"
 HOST="https://YOURDOMAIN:3000"
-dash="bed-dashboard-b8-27-eb-8d-ae-69-beddot2"
+dash="DASHBOARD NAME"
 mkdir dashboards 
 curl -k -H "Authorization: Bearer $KEY" $HOST/api/dashboards/db/$dash > dashboards/$dash.json 
-#curl -k -H "Authorization: Bearer $KEY" $HOST/api/dashboards/db/$dash | sed 's/"id":[0-9]\+,/"id":0,/' | sed 's/\(.*\)}/\1,"overwrite": true}/' > dashboards/$dash.json
